189f4a0fdfd0c576be6a7ff2c3755c3e4051cf7a
Vanik Egoyan <you@example.com>
2021-09-01 22:13:34 +0300

    done

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -4,7 +4,8 @@ const modals = () => {
         const trigger = document.querySelectorAll(triggerSelector),
               modal = document.querySelector(modalSelector),
               close = document.querySelector(closeSelector),
-              windows = document.querySelectorAll('[data-modal]');
+              windows = document.querySelectorAll('[data-modal]'),
+              scroll = calcScroll();
 
         trigger.forEach(item => {
             item.addEventListener('click', (e) => {
@@ -18,6 +19,7 @@ const modals = () => {
     
                 modal.style.display = "block";
                 document.body.style.overflow ="hidden";
+                document.body.style.marginRight = `${scroll}px`;//добавляем margin-rigth
                 // document.body.classList.add('modal-open');
             });
         });
@@ -29,6 +31,7 @@ const modals = () => {
 
             modal.style.display = "none";
             document.body.style.overflow ="";
+            document.body.style.marginRight = '0px';
             // document.body.classList.remove('modal-open');
         });
 
@@ -40,6 +43,7 @@ const modals = () => {
 
                 modal.style.display = "none";
                 document.body.style.overflow ="";
+                document.body.style.marginRight = '0px';
                 // document.body.classList.remove('modal-open');
             }
         });
@@ -52,13 +56,30 @@ const modals = () => {
         }, time);
     }
 
+    function calcScroll() {
+        let div = document.createElement('div');
+
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div); //помещаем блок на страницу
+
+        // вычисляем ширину
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scrollWidth;
+    }
+
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
     bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
     bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
     bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
 
-    // showModalByTime('.popup', 60000);
+    // showModalByTime('.popup', 6000);
 };
 
 export default modals;
